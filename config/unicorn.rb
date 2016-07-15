ab5c934be69caeef14f3b5477bf5238a3926ada7
Sivakumar V <sivakumar.bdu@gmail.com>
2016-07-15 14:48:55 +0530

    update unicorn conf

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,33 +1,22 @@
-# Set your full path to application.
-app_dir = File.expand_path('../../', __FILE__)
-shared_dir = File.expand_path('../../../shared/', __FILE__)
-
-# Set unicorn options
-worker_processes 4
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
 preload_app true
-timeout 120
-
-# Fill path to your app
-working_directory app_dir
-
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
-    begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
   end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
 end
 
-after_fork do |_server, _worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-end
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
 
-before_exec do |_server|
-  ENV['BUNDLE_GEMFILE'] = "#{app_dir}/Gemfile"
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
